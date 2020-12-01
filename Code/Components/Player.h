f873cd62008fb40a14962a594cd59e28f1071c52
Battledrake <battledrake@gmail.com>
2020-11-30 20:57:11 -0800

    First Commit

---
+++ Code/Components/Player.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <CryEntitySystem/IEntityComponent.h>
+#include <CryMath/Cry_Camera.h>
+
+#include <ICryMannequin.h>
+#include <CrySchematyc/Utils/EnumFlags.h>
+
+#include <DefaultComponents/Cameras/CameraComponent.h>
+#include <DefaultComponents/Input/InputComponent.h>
+#include <DefaultComponents/Audio/ListenerComponent.h>
+
+////////////////////////////////////////////////////////
+// Represents a player participating in gameplay
+////////////////////////////////////////////////////////
+class CPlayerComponent final : public IEntityComponent {
+	enum class EInputFlagType {
+		Hold = 0,
+		Toggle
+	};
+
+	enum class EInputFlag : uint8 {
+		MoveLeft = 1 << 0,
+		MoveRight = 1 << 1,
+		MoveForward = 1 << 2,
+		MoveBack = 1 << 3
+	};
+
+	static constexpr EEntityAspects InputAspect = eEA_GameClientD;
+
+public:
+	CPlayerComponent() = default;
+	virtual ~CPlayerComponent() = default;
+
+	// IEntityComponent
+	virtual void Initialize() override;
+
+	virtual Cry::Entity::EventFlags GetEventMask() const override;
+	virtual void ProcessEvent(const SEntityEvent& event) override;
+	// ~IEntityComponent
+
+	// Reflect type to set a unique identifier for this component
+	static void ReflectType(Schematyc::CTypeDesc<CPlayerComponent>& desc) {
+		desc.SetGUID("{63F4C0C6-32AF-4ACB-8FB0-57D45DD14725}"_cry_guid);
+	}
+
+protected:
+	void HandleInputFlagChange(CEnumFlags<EInputFlag> flags, CEnumFlags<EActionActivationMode> activationMode, EInputFlagType type = EInputFlagType::Hold);
+
+protected:
+	Cry::DefaultComponents::CCameraComponent* m_pCameraComponent = nullptr;
+	Cry::DefaultComponents::CInputComponent* m_pInputComponent = nullptr;
+	Cry::Audio::DefaultComponents::CListenerComponent* m_pAudioListenerComponent = nullptr;
+
+	CEnumFlags<EInputFlag> m_inputFlags;
+	Vec2 m_mouseDeltaRotation;
+};
---
+++ Code/GamePlugin.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <CrySystem/ICryPlugin.h>
+#include <CryGame/IGameFramework.h>
+#include <CryEntitySystem/IEntityClass.h>
+
+// The entry-point of the application
+// An instance of CGamePlugin is automatically created when the library is loaded
+class CGamePlugin
+	: public Cry::IEnginePlugin
+	, public ISystemEventListener {
+public:
+	CRYINTERFACE_SIMPLE(Cry::IEnginePlugin)
+		CRYGENERATE_SINGLETONCLASS_GUID(CGamePlugin, "Blank", "f01244b0-a4e7-4dc6-91e1-0ed18906fe7c"_cry_guid)
+
+		virtual ~CGamePlugin();
+
+	// Cry::IEnginePlugin
+	virtual const char* GetCategory() const override { return "Game"; }
+	virtual bool Initialize(SSystemGlobalEnvironment& env, const SSystemInitParams& initParams) override;
+	// ~Cry::IEnginePlugin
+
+	// ISystemEventListener
+	virtual void OnSystemEvent(ESystemEvent event, UINT_PTR wparam, UINT_PTR lparam) override;
+	// ~ISystemEventListener
+
+	// Helper function to get the CGamePlugin instance
+	// Note that CGamePlugin is declared as a singleton, so the CreateClassInstance will always return the same pointer
+	static CGamePlugin* GetInstance() {
+		return cryinterface_cast<CGamePlugin>(CGamePlugin::s_factory.CreateClassInstance().get());
+	}
+};
